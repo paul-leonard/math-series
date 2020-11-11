0cd16db097293250b2cf4af05c2c77b9a50751ab
paul-leonard <paul.ryan.leonard@gmail.com>
2020-11-10 18:46:34 -0800

    completed lucas function and passing tests

--- math_series/series.py
+++ math_series/series.py
@@ -1,7 +1,17 @@
 def fibonacci(n):
+  '''With a single input parameter of n, the function calculates and returns the Fibonacci Number.'''
   if n == 0:
     return 0
   elif n == 1:
     return 1
   else:
     return fibonacci(n-1) + fibonacci(n-2)
+
+def lucas(n):
+  '''With a single input parameter of n, the function calculates and returns the Lucas Number.'''
+  if n == 0:
+    return 2
+  elif n == 1:
+    return 1
+  else:
+    return lucas(n-1) + lucas(n-2)
--- tests/test_series.py
+++ tests/test_series.py
@@ -1,5 +1,7 @@
 from math_series.series import fibonacci
+from math_series.series import lucas
 
+######## Tests for fibonacci function ########
 def test_fibonacci_0():
   actual = fibonacci(0)
   expected = 0
@@ -33,4 +35,41 @@ def test_fibonacci_9():
 def test_fibonacci_12():
   actual = fibonacci(12)
   expected = 144
+  assert actual == expected
+
+
+######## Tests for lucas function ########
+def test_lucas_0():
+  actual = lucas(0)
+  expected = 2
+  assert actual == expected
+
+def test_lucas_1():
+  actual = lucas(1)
+  expected = 1
+  assert actual == expected
+
+def test_lucas_2():
+  actual = lucas(2)
+  expected = 3
+  assert actual == expected
+
+def test_lucas_3():
+  actual = lucas(3)
+  expected = 4
+  assert actual == expected
+
+def test_lucas_4():
+  actual = lucas(4)
+  expected = 7
+  assert actual == expected
+
+def test_lucas_9():
+  actual = lucas(9)
+  expected = 76
+  assert actual == expected
+
+def test_lucas_10():
+  actual = lucas(10)
+  expected = 123
   assert actual == expected
