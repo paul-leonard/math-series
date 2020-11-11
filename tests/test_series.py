ca6a2d8e31f19e0483a78594bed5f5451b5837a8
paul-leonard <paul.ryan.leonard@gmail.com>
2020-11-10 18:56:37 -0800

    added tests for sum_series function

--- tests/test_series.py
+++ tests/test_series.py
@@ -1,5 +1,7 @@
 from math_series.series import fibonacci
 from math_series.series import lucas
+#from math_series.series import sum_series
+
 
 ######## Tests for fibonacci function ########
 def test_fibonacci_0():
@@ -72,4 +74,125 @@ def test_lucas_9():
 def test_lucas_10():
   actual = lucas(10)
   expected = 123
+  assert actual == expected
+
+
+######## Tests for sum_series function ########
+######## for fibonacci w single argument ########
+def test_sum_series_0():
+  actual = sum_series(0)
+  expected = 0
+  assert actual == expected
+
+def test_sum_series_1():
+  actual = sum_series(1)
+  expected = 1
+  assert actual == expected
+
+def test_sum_series_2():
+  actual = sum_series(2)
+  expected = 1
+  assert actual == expected
+
+def test_sum_series_3():
+  actual = sum_series(3)
+  expected = 2
+  assert actual == expected
+
+def test_sum_series_4():
+  actual = sum_series(4)
+  expected = 3
+  assert actual == expected
+
+def test_sum_series_9():
+  actual = sum_series(9)
+  expected = 34
+  assert actual == expected
+
+def test_sum_series_12():
+  actual = sum_series(12)
+  expected = 144
+  assert actual == expected
+
+######## Tests for sum_series function ########
+######## for fibonacci w 3 arguments ########
+def test_sum_series_0():
+  actual = sum_series(0,0,1)
+  expected = 0
+  assert actual == expected
+
+def test_sum_series_1():
+  actual = sum_series(1,0,1)
+  expected = 1
+  assert actual == expected
+
+def test_sum_series_2():
+  actual = sum_series(2,0,1)
+  expected = 1
+  assert actual == expected
+
+def test_sum_series_3():
+  actual = sum_series(3,0,1)
+  expected = 2
+  assert actual == expected
+
+def test_sum_series_4():
+  actual = sum_series(4,0,1)
+  expected = 3
+  assert actual == expected
+
+def test_sum_series_9():
+  actual = sum_series(9,0,1)
+  expected = 34
+  assert actual == expected
+
+def test_sum_series_12():
+  actual = sum_series(12,0,1)
+  expected = 144
+  assert actual == expected
+
+
+######## Tests for sum_series function ########
+######## for lucas w 3 arguments ########
+def test_sum_series_0():
+  actual = sum_series(0,2,1)
+  expected = 2
+  assert actual == expected
+
+def test_sum_series_1():
+  actual = sum_series(1,2,1)
+  expected = 1
+  assert actual == expected
+
+def test_sum_series_2():
+  actual = sum_series(2,2,1)
+  expected = 3
+  assert actual == expected
+
+def test_sum_series_3():
+  actual = sum_series(3,2,1)
+  expected = 4
+  assert actual == expected
+
+def test_sum_series_4():
+  actual = sum_series(4,2,1)
+  expected = 7
+  assert actual == expected
+
+def test_sum_series_9():
+  actual = sum_series(9,2,1)
+  expected = 76
+  assert actual == expected
+
+def test_sum_series_10():
+  actual = sum_series(10,2,1)
+  expected = 123
+  assert actual == expected
+
+######## Tests for sum_series function ########
+######## for random 3 arguments ########
+
+def test_sum_series_10():
+  actual = sum_series(3,3,3)
+  expected != None
   assert actual == expected
